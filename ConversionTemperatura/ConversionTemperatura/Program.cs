5f41939a1bb21e7e94e69a9ea1201796256429b4
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-07 16:02:29 -0600

    conversion

--- ConversionTemperatura/ConversionTemperatura/Program.cs
+++ ConversionTemperatura/ConversionTemperatura/Program.cs
@@ -10,12 +10,22 @@ namespace ConversionTemperatura
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hola PROYECTO");
             Console.WriteLine("Lizzie Cerna");
             Console.WriteLine("Fredy Cerna");
 
             Console.WriteLine("Ingresa un nuvero");
-            Console.ReadLine(); 
+            int num = int.Parse(Console.ReadLine());
+
+            double multi = 1.8;
+            int grado = 32;
+            int resultado = (int)(num * multi + grado);   
+           
+            int multiplicacion = (int)(num * multi);
+
+            Console.WriteLine("Los grados Fahrenheit son =" + resultado);
+            Console.ReadLine();
 
             Console.ReadKey();
         }
