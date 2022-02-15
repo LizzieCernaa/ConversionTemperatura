167b4ebb34f5aabccd246d7d84766ce4a1270dc4
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-15 15:27:59 -0600

    nombre

--- ConversionTemperatura/ConversionTemperatura/Program.cs
+++ ConversionTemperatura/ConversionTemperatura/Program.cs
@@ -14,7 +14,7 @@ namespace ConversionTemperatura
             Console.WriteLine("Hola PROYECTO");
             Console.WriteLine("Lizzie Cerna");
             Console.WriteLine("Fredy Cerna");
-            Console.WriteLine("Hola buen día");
+            Console.WriteLine("Hola buen día Lizzie");
 
             Console.WriteLine("Ingresa un nuvero");
             int num = int.Parse(Console.ReadLine());
