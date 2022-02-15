40f8e93ec47d7f9e8b1bb86c90bbe722facb974b
fredy.cerna <fredycerna@gmail.com>
2022-02-15 15:26:35 -0600

    agregando nombre

--- ConversionTemperatura/ConversionTemperatura/Program.cs
+++ ConversionTemperatura/ConversionTemperatura/Program.cs
@@ -14,7 +14,7 @@ namespace ConversionTemperatura
             Console.WriteLine("Hola PROYECTO");
             Console.WriteLine("Lizzie Cerna");
             Console.WriteLine("Fredy Cerna");
-            Console.WriteLine("Hola buen día");
+            Console.WriteLine("Hola buen día Fredy");
 
             Console.WriteLine("Ingresa un nuvero");
             int num = int.Parse(Console.ReadLine());
